7c41ab9ea27430b4bda470f2bcc22cba02301000
supensa <stitch@Mobap.local>
2016-12-16 20:05:15 +1100

    UI Design

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Spencer Forrest on 16/12/2016.
+//  Copyright © 2016 Spencer Forrest. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var Thumb: UIImageView!
+    @IBOutlet weak var Title: UILabel!
+    @IBOutlet weak var Price: UILabel!
+    @IBOutlet weak var Details: UILabel!
+    
+    
+}
